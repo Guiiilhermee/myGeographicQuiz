0779e366dc17a413ea6ec388bd77bb6ac6897e7e
Guiiilhermee <guilherme_rodrigues91@hotmail.com>
2023-06-11 16:26:30 +0100

    Taking out feedback button

--- index.js
+++ index.js
@@ -85,10 +85,7 @@ const questionFunctions = {
       </p>
 
       <button type="button" class="quiz__total-return">Start again</button>
-
-      <button type="button" class="quiz__feedback">Send feedback</button>
-
-    `
+      `
 
     total.innerHTML = html
 
